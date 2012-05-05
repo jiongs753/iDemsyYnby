e7085ec408aaf091a54f42cdf8ed246ceaa71a61
kmetop <119588911@qq.com>
2012-05-05 17:05:21 +0800

    Initial Commit

---
+++ iDemsyYnby/DemsyAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DemsyAppDelegate.h
+//  iDemsyYnby
+//
+//  Created by yongshan ji on 12-5-5.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DemsyAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iDemsyYnby/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iDemsyYnby
+//
+//  Created by yongshan ji on 12-5-5.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DemsyAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DemsyAppDelegate class]));
+    }
+}
